ca7c7b8f655b0f8aca96121dfe613e472a0f0427
RANJITH <73814077+ranjithsys@users.noreply.github.com>
2022-08-12 22:07:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,53 +22,60 @@ sample.append(input,btn);
 document.body.append(sample);
 
 
+
+
 // async await function
 async function hackathon(){
     //the name from user//
-var res=document.getElementById("name").value;
 
+var res=document.getElementById("name").value;
+try{ 
 
 if(res==='michael'||res==='matthew'||res==='jane'){
-   
+  
     var url=`https://api.nationalize.io/?name=${res}`;
 var get=await fetch(url);
 var result=await get.json();
 //it gives the complete api details//
 // console.log(result);
-   }
-
-
 //throw error if any occur//
+}
+
 else{
-    alert("This is Not a valid Name");
+  alert("This Is Not a Valid Name..");
 }
 // storing the object value of key country in variable a//  
 var a=result.country;
-console.log(a);
+//  console.log(a);
 for (var i = 0; i <a.length-1; i++) {
   
    
-    var div=document.createElement("div");
-    div.setAttribute("class","final")
-    div.style.textAlign="center";
-    div.innerHTML=`<div class="card text-white bg-info mb-3" style="width: 18rem; ">
-    <div class="card-body">
-    <h3>NATIONALITY :</h3><br>
-    
-    <h5 style="align-center">${a[i].country_id}</h5>
-    <hr class="line">
-      <h3 class="heading">PORABABILITY:</h3><br>
-    
-      <h5 class="ptobability"> ${a[i].probability}</h5>
-      </div>
-  </div>`;
-  document.body.append(div);
+  var div=document.createElement("div");
+  div.setAttribute("class","final")
+  div.style.textAlign="center";
+  div.innerHTML=`<div class="card text-white bg-info mb-3" style="width: 18rem; ">
+  <div class="card-body">
+  <h3>NATIONALITY :</h3><br>
+  
+  <h5 style="align-center">${a[i].country_id}</h5>
+  <hr class="line">
+    <h3 class="heading">PORABABILITY:</h3><br>
+  
+    <h5 class="ptobability"> ${a[i].probability}</h5>
+    </div>
+</div>`;
+document.body.append(div);
 
 
 
 }
 
 
-
+ } //try end
+ catch(error){
+    console.log(error);
+    console.log("the name is not found");
 }
+}
+
 
